64f8e68d023ab937986b7ae8d62900313485d254
dixit-z <dixit.tr@gmail.com>
2018-03-01 20:36:54 +0100

    Algo

--- HashCode/src/com/porcasse/goninjagoninja/Algo.java
+++ HashCode/src/com/porcasse/goninjagoninja/Algo.java
@@ -10,7 +10,7 @@ import com.porcasse.kowabunga.Ride;
  *
  */
 public class Algo {
-	
+
 	private List<PairRide> ridesLesPlusOptis;
 	
 	private Integer maximumDistanceOpti = null; 
@@ -26,10 +26,13 @@ public class Algo {
 		Integer nbTrajetAOptimiser = nbRide - nbVehicules;
 		
 		if(nbRide > nbVehicules){
-		for(int i=0; i < rides.size(); i++){
-			comparerRideToList(rides.get(i),rides,i,nbTrajetAOptimiser);	
+			for(int i=0; i < rides.size(); i++){
+				comparerRideToList(rides.get(i),rides,i,nbTrajetAOptimiser);	
+			}
 		}
-		}
+		
+		
+		
 	}
 
 	private Integer comparerRideToList(Ride currentRide,List<Ride> rides, Integer index, Integer nbTrajetsAOptimiser) {
@@ -39,6 +42,8 @@ public class Algo {
 			if(i != index){
 				Integer distance = Utils.calculerDistanceVecteur(currentRide.getEnd(), rides.get(i).getStart());
 				if(ridesLesPlusOptis.size() < nbTrajetsAOptimiser){
+//					currentRide.setLocked(Boolean.TRUE);
+//					rides.get(i).setLocked(Boolean.TRUE);
 					ridesLesPlusOptis.add(new PairRide(currentRide, rides.get(i), distance));
 					nbDansListeOptis++;
 					if(maximumDistanceOpti == null || maximumDistanceOpti < distance){
@@ -47,11 +52,22 @@ public class Algo {
 				}
 				else if(maximumDistanceOpti < distance){
 					maximumDistanceOpti = distance;
+//					currentRide.setLocked(Boolean.TRUE);
+//					rides.get(i).setLocked(Boolean.TRUE);
 					ridesLesPlusOptis.add(new PairRide(currentRide, rides.get(i), distance));
 					nbDansListeOptis++;
 				}
 			}
 		}
 		return nbDansListeOptis;
+	}
+	
+	List<PairRide> removeDistancePair(Integer distance){
+		for(PairRide currentRide : ridesLesPlusOptis){
+			if(currentRide.getDelai() == distance){
+//				ridesLesPlusOptis.remove();
+			}
+		}
+		return null;
 	}
 }
