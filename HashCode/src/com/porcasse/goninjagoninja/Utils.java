85fdd76cb90fa84ee12c79300de40ab571e7f128
Julien Stefani <julien.stefani@soprasteria.com>
2018-03-01 20:24:55 +0100

    Fichier de sortie

--- HashCode/src/com/porcasse/goninjagoninja/Utils.java
+++ HashCode/src/com/porcasse/goninjagoninja/Utils.java
@@ -1,6 +1,14 @@
 package com.porcasse.goninjagoninja;
 
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
 import com.porcasse.kowabunga.Coordonnees;
+import com.porcasse.kowabunga.Ride;
 
 public class Utils {
 	
@@ -11,6 +19,21 @@ public class Utils {
 	 */
 	public static Integer calculerDistanceVecteur(Coordonnees A, Coordonnees B){
 		return (A.getX() - B.getX()) + (A.getY() - B.getY());
+	}
+	
+	public  static void creerFichierSortie(Map<Vehicule, List<Ride>> listeRideParVehicule) {
+		List<String> listToReturn = new List<String>();
+		for(Map.Entry<String, List<Ride>> entry : listeRideParVehicule.entrySet()) {
+			String line = "";
+			line += entry.getKey();
+			for(Map.Entry<String, String> rides : entry.getValue()) {
+				line += " ";
+				line += rides.getValue().toString();
+			}
+			listToReturn.add(line);
+		}
+		Path file = Paths.get("output.txt");
+		Files.write(file, listToReturn, Charset.forName("UTF-8"));
 	}
 	
 }
